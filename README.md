1fcebda501a6fe4eea08350079ac7bf652acfe74
Leonardo Viva <leonardo.leal@konecty.com>
2019-09-23 14:25:48 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Validate.js integration node
+
+Validate your data just as one would using [validate js](https://validatejs.org).
+
+
+## Usage
+
+- Set the property in wich your data will come.
+- Create your constraints in the editor or input it from `msg.template`.
+  
+![example](https://i.ibb.co/Y897z1Y/a.png)
---
+++ validate.js
@@ -0,0 +1,48 @@
+var validate = require("validate.js");
+
+module.exports = function(RED) {
+	"use strict";
+
+	function TemplateNode(config) {
+		RED.nodes.createNode(this, config);
+		const node = this;
+
+		this.field = config.field || "payload";
+		this.template = config.validate;
+		this.fieldType = config.fieldType || "msg";
+
+		node.on("input", function(msg) {
+			node.status({});
+
+			try {
+				// Use msg.template property if template editor is empty
+				if (msg.hasOwnProperty("template")) {
+					if (this.template == "" || this.template === null) {
+						this.template = msg.template;
+					}
+				}
+
+				const data = RED.util.evaluateNodeProperty(this.field, this.fieldType, this, msg);
+				const constraints = JSON.parse(this.template);
+
+				const result = validate(data, constraints, { format: "grouped" });
+				if (Object.keys(result || {}).length > 0) {
+					node.send([null, { ...msg, payload: result }]);
+					return;
+				}
+
+				node.send([msg, null]);
+			} catch (e) {
+				node.error(e);
+				node.status({
+					fill: "red",
+					shape: "ring",
+					text: "Error in validation"
+				});
+				node.send([null, e]);
+			}
+		});
+	}
+
+	RED.nodes.registerType("validate", TemplateNode);
+};
