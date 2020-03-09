78277f728647c058653c576ce741d1391e4a2a46
Leonardo Viva <leonardo.leal@konecty.com>
2020-03-09 11:53:19 -0300

    Add support to email validation in arrays

--- validate.js
+++ validate.js
@@ -11,6 +11,24 @@ validate.extend(validate.validators.datetime, {
     }
 });
 
+
+validate.validators.email = function(value, options) {
+    var pattern = /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/i;
+    var message = options.message || this.message || "is not a valid email";
+
+    if (!validate.isDefined(value)) {
+        return;
+    }
+    if (options.array && validate.isArray(value)) {
+        var invalid = value.some(e => !pattern.exec(e));
+        if (invalid) return message;
+        return;
+    }
+    if (!pattern.exec(value)) {
+        return message;
+    }
+};
+
 module.exports = function(RED) {
 	"use strict";
 
